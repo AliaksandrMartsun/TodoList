694ebc24c8562f9c5c3adc595a691fc16d9488ef
AliaksandrMartsun <Aliaksandr.martsun@gmail.com>
2024-01-29 14:36:00 +0300

    new

--- src/main/java/com/example/TodoList/persist/repo/ToDoRepository.java
+++ src/main/java/com/example/TodoList/persist/repo/ToDoRepository.java
@@ -1,6 +1,5 @@
 package com.example.TodoList.persist.repo;
 
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +13,7 @@ import java.util.List;
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
 
     @Query("select new com.example.TodoList.repr.ToDoRepr(t.id, t.description, t.user.username, t.targetDate) " +
-            "from ToDo t " +
+            "from com.example.TodoList.persist.entity.ToDo t" +
             "where t.user.id = :userId")
     List<ToDoRepr> findToDosByUserId(@Param("userId") Long userId);
 }
