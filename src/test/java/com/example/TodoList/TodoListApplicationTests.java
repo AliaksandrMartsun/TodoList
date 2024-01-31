8f64a8121448578522a1cc35b9abd287139c28eb
AliaksandrMartsun <Aliaksandr.martsun@gmail.com>
2024-01-31 13:58:06 +0300

    new bug fix in TodoController

--- src/test/java/com/example/TodoList/TodoListApplicationTests.java
+++ src/test/java/com/example/TodoList/TodoListApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.TodoList;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class TodoListApplicationTests {
+public class TodoListApplicationTests {
 
 	@Test
 	void contextLoads() {
